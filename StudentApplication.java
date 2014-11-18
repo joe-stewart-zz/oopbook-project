8b2554ebb4e35fe1a1ccc7951c74ef6181249de6
Joe Stewart <joe.stewart.dev@gmail.com>
2014-11-18 13:27:18 -0800

    modified StudentApplication to use University class

--- StudentApplication.java
+++ StudentApplication.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class StudentApplication {
-    private static Student[] students;
-    private static int noStudents;
+    private static University university;
     private static Scanner scanner;
+
     public static int menu() {
         int choice;
 
@@ -16,53 +16,57 @@ public class StudentApplication {
         choice = scanner.nextInt();
         return choice;
     }
+
     public static void choice1() {
-        String firstName, lastName;
-        String phone;
+        String firstName, lastName, phone;
+        Student student;
+
         System.out.println("Enter the student's first name, last name, and telephone number: ");
         firstName = scanner.next();
         lastName = scanner.next();
         phone = scanner.next();
-        students[noStudents] = new Student(firstName, lastName, phone);
-        noStudents++;
+
+        student = university.addStudent(firstName, lastName, phone);
+        System.out.println("StudentID: " + student.getID());
     }
+
     public static void choice2() {
         int studentID;
         Student student;
         System.out.println("Enter a student ID: ");
         studentID = scanner.nextInt();
-        student = getStudent(studentID);
+
+        student = university.getStudent(studentID);
         if(student == null)
             System.out.println("No student with that ID.");
         else
             System.out.println(student.toString());
     }
+
     public static void choice3() {
         int studentID;
-        Student student;
+        String newPhone;
+        boolean changed;
+
         System.out.println("Enter a student ID: ");
         studentID = scanner.nextInt();
-        student = getStudent(studentID);
-        if(student == null)
+
+        System.out.println("Enter new telephone number: ");
+        newPhone = scanner.next();
+
+        changed = university.changePhone(studentID, newPhone);
+        if(!changed)
             System.out.println("No student with that ID.");
-        else {
-            String newPhone;
-            System.out.println("Enter the new telephone number: ");
-            newPhone = scanner.next();
-            student.setPhone(newPhone);
-        }
     }
+
     public static void choice4() {
-        System.out.println("Student List:");
-        for(int i = 0; i < noStudents; i++)
-            System.out.println(students[i].toString());
-        System.out.println("\n");
+        System.out.println(university.getStudents());
     }
+
     public static void main(String[] args) {
         int choice;
 
-        students = new Student[100];
-        noStudents = 0;
+        university = new University();
         scanner = new Scanner(System.in);
 
         choice = menu();
@@ -76,12 +80,5 @@ public class StudentApplication {
             }
             choice = menu();
         };
-    }
-    public static Student getStudent(int studentID) {
-        for(int i = 0; i < noStudents; i++) {
-            if(students[i].getID() == studentID)
-                return students[i];
-        }
-        return null;
     }
 }
